04954870eadd595a20d80b0c9f6edb2b752262b1
Blake Mallory <blakecmallory@gmail.com>
2024-01-16 12:23:09 -0500

    increased timeout and increased number of tokens

--- query_api.py
+++ query_api.py
@@ -5,8 +5,8 @@ parser.add_argument("-f", "--file", help="input file",required=True)
 args = parser.parse_args()
 
 
-client = Client("http://127.0.0.1:8080",timeout=100)
+client = Client("http://127.0.0.1:8080",timeout=180)
 with open(args.file, "r") as f:
     prompt = f.read()
-print(client.generate(prompt, max_new_tokens=256).generated_text)
+print(client.generate(prompt, max_new_tokens=1024).generated_text)
 
